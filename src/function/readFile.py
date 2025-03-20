eac5b861608d803dd6d2a7d72f1cfa1421dd6fbf
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-03-20 15:54:10 -0300

    fix: alterando a leitura de temperatura para numeros inteiros

--- src/function/readFile.py
+++ src/function/readFile.py
@@ -7,7 +7,7 @@ def readFile(file_path) -> list:
         arquivo = file.read().split("\n")
         for linha in arquivo[1:-2]:
             dado = linha.split(";")
-            obj = Temperatura(dado[0], float(dado[1].replace(",", ".")))
+            obj = Temperatura(dado[0], int(float(dado[1].replace(",", "."))))
             dados.append(obj)
     
     return dados
